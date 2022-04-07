f4cc234efe9038cd2bfc08aba782501238f4f4a9
Matthew Stewart <catcherdbacks@msn.com>
2022-04-07 00:19:09 -0400

    Created the models for User and UnlockUser across domains

---
+++ src/ActiveDirectory.Lib/Models/ActiveDirectoryUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectory.Lib.Models
+{
+    public class ActiveDirectoryUser
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+        public string Path { get; set; }
+        public bool IsLockedOut { get; set; }
+        public string EmployeeId { get; set; }
+        public string PasswordLastSet { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool IsExpired { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Office { get; set; }
+        public string StreetAddress { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+        public string HomeDirectory { get; set; }
+        public string Location
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(City))
+                {
+                    return Office;
+                }
+
+                return $"{ Office } - { City }, { State }";
+            }
+        }
+        public string Notes { get; set; }
+        public string Manager { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ src/ActiveDirectory.Lib/Models/UnlockUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectory.Lib.Models
+{
+    public class UnlockUserModel
+    {
+        public string Message { get; set; }
+        public bool IsUnlocked { get; set; }
+    }
+}
