f2ac5b86c6e6fb21624b0a247f6c4e0d740d5867
Matthew Stewart <catcherdbacks@msn.com>
2022-04-07 00:20:57 -0400

    Created a random password generator

---
+++ src/ActiveDirectory.Lib/PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActiveDirectory.Lib.PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        private static string GetChars()
+        {
+            List<char> chars = new();
+
+            for (char i = '0'; i < '9'; i++)
+            {
+                chars.Add(i);
+            }
+
+            for (char i = 'A'; i < 'Z'; i++)
+            {
+                chars.Add(i);
+            }
+
+            for (char i = 'a'; i < 'z'; i++)
+            {
+                chars.Add(i);
+            }
+
+            return new string(chars.ToArray());
+        }
+
+        private static string GenPassword(string chars, int len)
+        {
+            StringBuilder sb = new();
+            var rand = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                sb.Append(chars[rand.Next(chars.Length)]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static string GeneratePassword(int length)
+        {
+            var chars = GetChars();
+            bool hasNumber = false;
+            bool hasCapital = false; ;
+            string password = string.Empty;
+            while (hasNumber == false || hasCapital == false)
+            {
+                password = GenPassword(chars, length);
+                hasNumber = HasNumber(password);
+                hasCapital = HasCapital(password);
+            }
+
+            return password;
+        }
+
+        private static bool HasNumber(string password)
+        {
+            foreach (var c in password)
+            {
+                if (int.TryParse(c.ToString(), out _))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool HasCapital(string password)
+        {
+            foreach (var c in password)
+            {
+                if (c >= 65 && c <= 90)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
