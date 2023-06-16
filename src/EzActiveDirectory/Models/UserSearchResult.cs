49360bd791ca935a07054a3468eca5b0a4995ac6
Matthew Stewart <catcherdbacks@msn.com>
2023-06-16 02:34:39 -0400

    Created a collection as a dictionary for the searched user results. Create user Search result for the data and incorporated them into the AD implementation.

---
+++ src/EzActiveDirectory/Models/UserSearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Text;
+
+namespace EzActiveDirectory.Models;
+public class UserSearchResult
+{
+    public ResultPropertyValueCollection Result{ get; set; }
+    public bool CheckedResult { get; set; }
+}
