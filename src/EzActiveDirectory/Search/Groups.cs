f68e2b44e9b40422150a01afb6597abf1c1972a3
Matthew Stewart <60456321+mstew20@users.noreply.github.com>
2024-10-26 22:29:51 -0400

    Cleanup and organize code (#6)
    * Adding IAsyncEnumerable for the unlock tool option
    
    * added library for async enum and removed old way
    
    * removed uneeded allocation
    
    * upgrading to .net7 and updating packages and removed no longer needed packageds
    
    * Cleaned up messeages and warnings using a supressions for platforms specific errors.
    
    * updated project to .net8
    
    * Cleaned up messages
    
    * removed a no longer used event.
    
    * Delete src/ADTesting directory
    
    * Added Bitlocker Recovery
    
    * added getting a single group by the group path
    
    * Delete src/ADTesting directory (#5)
    
    this should not be here
    
    * removed project from Solution
    
    * Created seperate classes for each search function to organize code.
    
    * further refactoring to seperate each funtion to its own class and accessible property in ActiveDirectory class
    
    * missed adding the computer functions to the AD class
    
    * added missed check for created domain if LDAP doesn't provide server with the path
    
    * moved an AD not user method for getting domain controllers out of users
    
    * wrong property name for computer search

---
+++ src/EzActiveDirectory/Search/Groups.cs
@@ -0,0 +1,87 @@
+﻿using EzActiveDirectory.Models;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+
+namespace EzActiveDirectory.Search;
+public class Groups
+{
+    private readonly ActiveDirectory _ad;
+
+    public Groups(ActiveDirectory ad)
+    {
+        _ad = ad;
+    }
+
+    public bool RemoveUser(string userPath, string groupPath, UserCredentials credentials = null)
+    {
+        try
+        {
+            using var deGroup = _ad.GetDirectoryEntry(groupPath, credentials);
+            deGroup.Invoke("Remove", new object[] { userPath });
+            deGroup.CommitChanges();
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+    public bool AddUser(string userPath, string groupPath, UserCredentials credentials = null)
+    {
+        try
+        {
+            using var deGroup = _ad.GetDirectoryEntry(groupPath, credentials);
+            deGroup.Invoke("Add", new object[] { userPath });
+            deGroup.CommitChanges();
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        return true;
+    }
+    public List<ActiveDirectoryGroup> Find(string groupName, UserCredentials credentials = null)
+    {
+        List<ActiveDirectoryGroup> groups = [];
+
+        try
+        {
+            using var directoryEntry = _ad.GetDirectoryEntry(_ad.LdapPath, credentials);
+            directoryEntry.RefreshCache();
+            using DirectorySearcher deSearcher = new(directoryEntry);
+            deSearcher.Filter = $"(&(objectClass=group)(name=*{groupName}*))";
+            deSearcher.SearchScope = SearchScope.Subtree;
+            var results = deSearcher.FindAll();
+
+            foreach (SearchResult group in results)
+            {
+                groups.Add(new()
+                {
+                    Path = group.Path,
+                    Name = group.Properties["name"]?.Value().ToString(),
+                    Description = group.Properties["description"].GetValue<string>(),
+                    Notes = group.Properties["info"].GetValue<string>()
+                });
+            }
+        }
+        catch (Exception) { }
+
+        return groups;
+    }
+    public ActiveDirectoryGroup Get(string groupPath, UserCredentials credentials = null)
+    {
+        using var directoryEntry = _ad.GetDirectoryEntry(groupPath, credentials);
+        ActiveDirectoryGroup group = new()
+        {
+            Path = directoryEntry.Path,
+            Name = directoryEntry.Properties[Property.Name].Value?.ToString(),
+            Description = directoryEntry.Properties["description"].Value?.ToString(),
+            Notes = directoryEntry.Properties[Property.Notes].Value?.ToString()
+        };
+
+        return group;
+    }
+}
