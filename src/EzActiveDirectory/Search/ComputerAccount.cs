27646108a6242fdb82f145af6aca019d224ec1db
Matthew Stewart <catcherdbacks@msn.com>
2024-11-30 22:04:45 -0500

    Revert "Added Computer Search"
    This reverts commit 0a6e51adfb7ade2d7c9bc658eef3f256c696155e.

--- src/EzActiveDirectory/Search/ComputerAccount.cs
+++ src/EzActiveDirectory/Search/ComputerAccount.cs
@@ -1,54 +1,31 @@
 ﻿using EzActiveDirectory.Models;
-using System.Collections.Generic;
 using System.DirectoryServices;
 
 namespace EzActiveDirectory.Search;
 public class ComputerAccount
 {
-	private readonly ActiveDirectory _ad;
+    private readonly ActiveDirectory _ad;
 
-	public ComputerAccount(ActiveDirectory ad)
-	{
-		_ad = ad;
-	}
+    public ComputerAccount(ActiveDirectory ad)
+    {
+        _ad = ad;
+    }
 
-	public Computer Find(string compName, UserCredentials credentials = null)
-	{
-		using var directoryEntry = _ad.GetDirectoryEntry(_ad.LdapPath, credentials);
-		DirectorySearcher searcher = new(directoryEntry)
-		{
-			Filter = $"(&(objectClass=computer)(cn=*{compName}*))"
-		};
-		var result = searcher.FindOne();
+    public Computer Find(string compName, UserCredentials credentials = null)
+    {
+        using var directoryEntry = _ad.GetDirectoryEntry(_ad.LdapPath, credentials);
+        DirectorySearcher searcher = new(directoryEntry)
+        {
+            Filter = $"(&(objectClass=computer)(cn=*{compName}*))"
+        };
+        var result = searcher.FindOne();
 
-		var comp = new Computer()
-		{
-			Name = result.Properties["name"].GetValue<string>(),
-			Path = result.Properties["adspath"].GetValue<string>(),
-		};
+        var comp = new Computer()
+        {
+            Name = result.Properties["name"].GetValue<string>(),
+            Path = result.Properties["adspath"].GetValue<string>(),
+        };
 
-		return comp;
-	}
-
-	public List<Computer> FindAll(string compName, UserCredentials credentials = null)
-	{
-		using var directoryEntry = _ad.GetDirectoryEntry(_ad.LdapPath, credentials);
-		DirectorySearcher searcher = new(directoryEntry)
-		{
-			Filter = $"(&(objectClass=computer)(cn=*{compName}*))"
-		};
-		var result = searcher.FindAll();
-		List<Computer> output = [];
-		foreach (SearchResult r in result)
-		{
-			var comp = new Computer()
-			{
-				Name = r.Properties["name"].GetValue<string>(),
-				Path = r.Properties["adspath"].GetValue<string>(),
-			};
-			output.Add(comp);
-		}
-
-		return output;
-	}
+        return comp;
+    }
 }
